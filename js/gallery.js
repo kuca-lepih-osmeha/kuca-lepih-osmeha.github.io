72d7e78b1f025809a4a80c4c33404cdf5809e266
Vladimir Radinovic <Vladimir.Radinovic@nortal.com>
2016-08-21 01:34:35 +0200

    Add first version

---
+++ js/gallery.js
@@ -0,0 +1,17 @@
+
+;(function ($, window, document, undefined) {
+
+    // var toggleGallery = function(galleryName) {
+    //     console.log(galleryName);
+    //     $('.elastislide-wrapper').parent().hide();
+    //     $("#" + galleryName).parent().parent().parent().show();
+    // };
+    //
+    // $(document).on('click', '.gallery_toggle', function (e) {
+    //     e.preventDefault();
+    //     toggleGallery($(e.currentTarget).data('gallery-name'));
+    // });
+    //
+    // toggleGallery('office_carousel');
+
+})(jQuery, window, document);
